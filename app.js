92e6543ca6ff24b906f1a822c0238092d1b80d0a
cjfloresvera <cj.floresvera@gmail.com>
2025-01-30 04:27:20 -0300

    Incluye incorporación de nombres en el HTML desde el Arreglo

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
-// Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos=[];
-
+let nombreAmigo=""
+let tamaño=""
+let linea=""
 //Agrega amigos a la lista
 //REVISAR PORQUÉ ME SALE ERROR DE TIEMPO
 function agregarAmigo(){
@@ -10,7 +11,19 @@ function agregarAmigo(){
         listaAmigos.push(amigo);
         console.log(listaAmigos);
         document.getElementById('amigo').value = "";
-        } else {
+        tamaño=listaAmigos.length;
+        console.log(tamaño);
+       //Para crear la linea en el html por cada item del arreglo:
+        for(var i=tamaño-1;i<=tamaño-1;i++){
+          linea=document.createElement('li')
+          linea.textContent=listaAmigos[i];
+          console.log(linea);
+          listadoHTML.appendChild(linea);          
+        }
+      } else {
             alert("Por favor ingrese un nombre válido");
         }
 }
+
+let listadoHTML=document.querySelector('#listaAmigos');
+listadoHTML.innerHTML='';
