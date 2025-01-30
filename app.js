01a965a2d988965799ad63eb667b93ca1b012ab0
cjfloresvera <cj.floresvera@gmail.com>
2025-01-30 05:59:24 +0000

    Valida y agrega amigos al array

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos=[];
 
+//Agrega amigos a la lista
+//REVISAR PORQUÉ ME SALE ERROR DE TIEMPO
 function agregarAmigo(){
-   listaAmigos.push(document.getElementById('Añadir')); 
+    let amigo=document.getElementById('amigo').value
+    if(amigo!=""){
+        listaAmigos.push(amigo);
+        console.log(listaAmigos);
+        document.getElementById('amigo').value = "";
+        } else {
+            alert("Por favor ingrese un nombre válido");
+        }
 }
