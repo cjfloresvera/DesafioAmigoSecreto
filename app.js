6e315814270e2ed1eaccc0373c0f8a69c5610f6d
cjfloresvera <cj.floresvera@gmail.com>
2025-02-18 20:20:44 -0300

    Simplificación código innecesario

--- app.js
+++ app.js
@@ -1,48 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos=[];
-let nombreAmigo=""
-let tamaño=""
 let linea=""
-
+let listadoHTML=document.querySelector('#listaAmigos');
+listadoHTML.innerHTML='';
+let amigoSecreto=''
 //Agrega amigos a la lista
 //REVISAR PORQUÉ ME SALE ERROR DE TIEMPO
 function agregarAmigo(){
     let amigo=document.getElementById('amigo').value
+    console.log(typeof(amigo));
     if(amigo!=""){
         listaAmigos.push(amigo);
-        console.log(listaAmigos);
         document.getElementById('amigo').value = "";
-        tamaño=listaAmigos.length;
-        console.log(tamaño);
        //Para crear la linea en el html por cada item del arreglo:
-        for(var i=tamaño-1;i<=tamaño-1;i++){
           linea=document.createElement('li')
-          linea.textContent=listaAmigos[i];
-          console.log(linea);
+          linea.textContent=listaAmigos[listaAmigos.length-1];
           listadoHTML.appendChild(linea);          
+      } else { 
+          alert("Por favor ingrese un nombre válido"); 
         }
-      } else {
-        alert("Por favor ingrese un nombre válido"); 
-        }
-      return;
 }
-
-
-
-let listadoHTML=document.querySelector('#listaAmigos');
-listadoHTML.innerHTML='';
-
-let amigoSecreto=''
 
 //Para sortear el amigo secreto y mostrarlo
 function sortearAmigo(){
-  if(tamaño<=0){
-    alert("No tiene amigos en la lista")
+  if(listaAmigos.length<=0){
+    alert("No tiene amigos en la lista");
   } else{
-    let numeroAleatoreo=parseInt(Math.floor(Math.random()*tamaño));
-    console.log(numeroAleatoreo)
+    let numeroAleatoreo=parseInt(Math.floor(Math.random()*listaAmigos.length));
     amigoSecreto=listaAmigos[numeroAleatoreo]
-    console.log(amigoSecreto)
     listadoHTML.innerHTML=`¡El amigo secreto sorteado es: ${amigoSecreto}!`
     return 
   }
